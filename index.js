a2e6b27ec5b3a08335bd3140078b594b9a904eb4
Harshal R Kitukale <107461692+harshal-kitukale@users.noreply.github.com>
2023-12-23 20:32:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const PORT=process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-  res.send('Hey this server is running')
-})
+// app.get("/",(req,res)=>{
+//   res.send('Hey this server is running')
+// })
 
 app.use("/user",userRouter)
 app.use(authMiddleware)
